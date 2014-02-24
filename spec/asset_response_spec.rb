30b9b6dd00221e07f88525765f80364a0ddded54
Steven Jack <stevenmajack@gmail.com>
2014-02-24 14:10:03 +0000

    Adds specs for asset response

---
+++ spec/asset_response_spec.rb
@@ -0,0 +1,49 @@
+require 'spec_helper'
+
+describe Alephant::Broker::AssetResponse do
+
+  describe "#initialize(request, config)" do
+    let(:config) { { :lookup_table_name => 'test_table', :bucket_id => 'test_bucket', :path => 'test_path' } }
+    let(:request) { double("Alephant::Broker::Request", :component_id => 'test', :content_type => 'text/html', :options => {:variant => 'test_variant'} ) }
+    let(:location) { 'test_location' }
+
+    it "Should return the content from a successful cache lookup" do
+      lookup_table = double('Alephant::Lookup::LookupTable')
+      allow(lookup_table).to receive(:read).with(request.options).and_return(location)
+      Alephant::Lookup.stub(:create).and_return(lookup_table)
+      Alephant::Cache.any_instance.stub(:initialize)
+      Alephant::Cache.any_instance.stub(:get).with(location).and_return('Test cache content')
+      instance = Alephant::Broker::AssetResponse.new(request, config)
+
+      expect(instance.content).to eq('Test cache content')
+      expect(instance.status).to eq(200)
+    end
+
+    it "should return a 404 if lookup can't find a valid location" do
+      lookup_table = double('Alephant::Lookup::LookupTable')
+      allow(lookup_table).to receive(:read).with(request.options).and_return(nil)
+
+      Alephant::Lookup.stub(:create).and_return(lookup_table)
+      Alephant::Cache.any_instance.stub(:initialize)
+      instance = Alephant::Broker::AssetResponse.new(request, config)
+
+      expect(instance.content).to eq('Cache key not found based on component_id and options combination')
+      expect(instance.status).to eq(404)
+    end
+
+    it "should return a 500 for any other exceptions" do
+      lookup_table = double('Alephant::Lookup::LookupTable')
+      allow(lookup_table).to receive(:read).with(request.options).and_raise(Exception)
+
+      Alephant::Lookup.stub(:create).and_return(lookup_table)
+      instance = Alephant::Broker::AssetResponse.new(request, config)
+
+      expect(instance.status).to eq(500)
+
+    end
+
+  end
+
+end
+
+
