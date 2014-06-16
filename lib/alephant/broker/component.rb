b1b1ba13acb5a4da26e468dcd3bdf12080ecfd02
Robert Kenny <kenoir@gmail.com>
2014-06-16 08:58:14 +0100

    flip mode

--- lib/alephant/broker/component.rb
+++ lib/alephant/broker/component.rb
@@ -6,61 +6,54 @@ require 'alephant/broker/errors/invalid_cache_key'
 require 'alephant/sequencer'
 require 'alephant/broker/cache'
 
+module Alephant::Broker
+  module Adapter
+    class Static
+      attr_reader :cache
 
-module Alephant
-  module Broker
-    class Component
-      class Adapter
-        attr_reader :cache
+      def initialize(cache_client)
+        @cache = cache_client
+      end
 
-        def initialize(cache_client)
-          @cache = cache_client
-        end
+      def load(id, batch_id, options)
+        opts_hash  = Crimp.signature(options)
+        sequencer  = sequencer_for "#{batch_id || id}/#{opts_hash}"
 
-        def load(id, batch_id, opts_hash)
-          {
-            :content   => s3.get(s3_path_for(id, options, version),
-            :opts_hash => opts_hash,
-            :version   => version
-          }
-        end
+        version    = sequencer.get_last_seen
+        path       = path_for(id, options, version)
 
-        def version
-          @version ||= sequencer.get_last_seen
-        end
+        {
+          :content   => datastore.get path,
+          :version   => version
+        }
+      end
 
-        def s3
-          @s3_cache ||= Alephant::Cache.new(
-            Broker.config[:s3_bucket_id],
-            Broker.config[:s3_object_path]
-          )
-        end
+      private
 
-        def s3_path_for(id,options,version)
-          lookup.read(id, options, version).tap do |lookup_object|
-            raise InvalidCacheKey if lookup_object.location.nil?
-          end.location unless version.nil?
-        end
+      def datastore
+        @datastore ||= Alephant::Cache.new(
+          Broker.config[:s3_bucket_id],
+          Broker.config[:s3_object_path]
+        )
+      end
 
-        def lookup
-          @lookup ||= Alephant::Lookup.create(Broker.config[:lookup_table_name])
-        end
+      def path_for(id, options, version)
+        lookup.read(lookup_key, options, version).tap do |lookup_object|
+          raise InvalidCacheKey if lookup_object.location.nil?
+        end.location unless version.nil?
+      end
 
-        def key
-          batch_id.nil? ? component_key : renderer_key
-        end
+      def lookup
+        @lookup ||= Alephant::Lookup.create(
+          Broker.config[:lookup_table_name]
+        )
+      end
 
-        def component_key
-          "#{id}/#{opts_hash}"
-        end
-
-        def renderer_key
-          "#{batch_id}/#{opts_hash}"
-        end
-
-        def sequencer
-          @sequencer ||= Alephant::Sequencer.create(Broker.config[:sequencer_table_name], key)
-        end
+      def sequencer_for(seq_key)
+        @sequencer ||= Alephant::Sequencer.create(
+          Broker.config[:sequencer_table_name],
+          seq_key
+        )
       end
     end
   end
@@ -80,7 +73,6 @@ module Alephant
         @batch_id  = batch_id
         @cache     = Cache::Client.new
         @options   = symbolize(options || {})
-        @opts_hash = Crimp.signature(options)
         @cached    = true
       end
 
