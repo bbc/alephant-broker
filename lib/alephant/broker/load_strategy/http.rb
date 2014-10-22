2cba1119781370ca9967bb0c4958340333eaf3b4
Dan Arnould <dan@arnould.co.uk>
2014-10-22 13:53:32 +0100

    HTTP Load Strategy
    Closes #17

---
+++ lib/alephant/broker/load_strategy/http.rb
@@ -0,0 +1,68 @@
+require 'alephant/broker/cache'
+require 'alephant/broker/errors/content_not_found'
+require 'faraday'
+
+module Alephant
+  module Broker
+    module LoadStrategy
+      class HTTP
+        class RequestFailed < StandardError; end
+
+        class URL
+          def generate
+            raise NotImplementedError
+          end
+        end
+
+        def initialize(url_generator)
+          @url_generator = url_generator
+        end
+
+        def load(component_meta)
+          cache_object(component_meta)
+        rescue
+          cache.set(component_meta.cache_key, content(component_meta))
+        end
+
+        private
+
+        attr_reader :cache, :url_generator
+
+        def cache
+          @cache ||= Cache::Client.new
+        end
+
+        def cache_object(component_meta)
+          cache.get(component_meta.cache_key) { content component_meta }
+        end
+
+        def content(component_meta)
+          resp = request component_meta
+          {
+            :content => resp.body,
+            :content_type => extract_content_type_from(resp.env.response_headers)
+          }
+        end
+
+        def extract_content_type_from(headers)
+          headers['content-type'].split(';').first
+        end
+
+        def request(component_meta)
+          component_meta.cached = false
+          Faraday.get(url_for component_meta).
+                  tap { |r| raise ContentNotFound unless r.success? }
+        rescue => e
+          raise RequestFailed, e
+        end
+
+        def url_for(component_meta)
+          url_generator.generate(
+            component_meta.id,
+            component_meta.options
+          )
+        end
+      end
+    end
+  end
+end
---
+++ spec/http_spec.rb
@@ -0,0 +1,87 @@
+require "spec_helper"
+
+describe Alephant::Broker::LoadStrategy::HTTP do
+  subject { described_class.new(url_generator) }
+
+  let(:component_meta) do
+    double('Alephant::Broker::ComponentMeta', cache_key: 'cache_key', id: 'test')
+  end
+  let(:url_generator) { double(generate: "http://foo.bar") }
+  let(:cache) { double('Alephant::Broker::Cache::Client') }
+  let(:body) { '<h1>Batman!</h1>' }
+  let(:content) do
+    {
+      :content => body,
+      :content_type => 'text/html'
+    }
+  end
+
+  before :each do
+    allow(Alephant::Broker::Cache::Client).to receive(:new) { cache }
+  end
+
+  describe "#load" do
+    context "content in cache" do
+      before :each do
+        allow(cache).to receive(:get) { content }
+      end
+
+      it "gets from cache" do
+        expect(subject.load component_meta).to eq content
+      end
+    end
+
+    context "content not in cache" do
+      before :each do
+        allow(cache).to receive(:get).and_yield
+        allow(component_meta).to receive(:'cached=').with(false) { false }
+        allow(component_meta).to receive(:options).and_return Hash.new
+      end
+
+      context "and available over HTTP" do
+        let(:env) { double('env', response_headers: response_headers) }
+        let(:response_headers) { {'content-type' => 'text/html; test'} }
+
+        before :each do
+          allow(Faraday).to receive(:get) do
+            instance_double(
+              'Faraday::Response',
+              body: body,
+              :'success?' => true,
+              env: env)
+          end
+         end
+
+        it "gets from HTTP" do
+         expect(subject.load component_meta).to eq content
+        end
+      end
+
+      context "and HTTP request fails" do
+        before :each do
+          allow(Faraday).to receive(:get).and_raise 
+        end
+
+        specify do
+          expect do
+            subject.load component_meta
+          end.to raise_error Alephant::Broker::LoadStrategy::HTTP::RequestFailed
+        end
+      end
+
+      context "and HTTP request 404s" do
+        before :each do
+          allow(Faraday).to receive(:get) do
+            instance_double('Faraday::Response', body: body, :'success?' => false)
+          end
+        end
+
+        specify do
+          expect do
+            subject.load component_meta
+          end.to raise_error Alephant::Broker::LoadStrategy::HTTP::RequestFailed
+        end
+      end
+    end
+  end
+end
