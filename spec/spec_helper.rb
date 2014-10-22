056893fcc2292bde3f6748284759b46e61fc32a9
Dan Arnould <dan@arnould.co.uk>
2014-10-22 13:53:32 +0100

    HTTP Load Strategy
    Closes #17
    Authors: @revett @darnould

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,10 @@ require 'pry'
 require 'json'
 require 'alephant/broker'
 require 'alephant/broker/load_strategy/s3'
+require "alephant/broker/load_strategy/http"
+require "alephant/broker/cache"
+require "alephant/broker/errors/content_not_found"
+
 require 'rack/test'
 
 ENV['RACK_ENV'] = 'test'
