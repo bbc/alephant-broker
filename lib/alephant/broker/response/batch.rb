5ecb47eb3b1b8488804809e10e00e36bf9e6a1b0
Charlie Revett <charlierevett@gmail.com>
2014-11-04 12:44:36 +0000

    ResponseBatch refactor

--- lib/alephant/broker/response/batch.rb
+++ lib/alephant/broker/response/batch.rb
@@ -26,7 +26,7 @@ module Alephant
 
         def json
           logger.info "Broker: Batch load started (#{batch_id})"
-          result = components.map do |component|
+          components.map do |component|
             {
               'component'    => component.id,
               'options'      => component.options,
@@ -34,10 +34,7 @@ module Alephant
               'content_type' => component.content_type,
               'body'         => component.content
             }
-          end
-          logger.info "Broker: Batch load done (#{batch_id})"
-
-          result
+          end.tap { logger.info "Broker: Batch load done (#{batch_id})" }
         end
 
       end
