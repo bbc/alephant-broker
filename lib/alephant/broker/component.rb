671f833d23e93f857a09585540f69c75bf0f3dc1
Steven Jack <stevenmajack@gmail.com>
2015-03-30 17:48:30 +0100

    Merge headers from s3 into headers sent back to component

--- lib/alephant/broker/component.rb
+++ lib/alephant/broker/component.rb
@@ -1,9 +1,9 @@
-require 'crimp'
-require 'alephant/cache'
-require 'alephant/lookup'
-require 'alephant/broker/errors/invalid_cache_key'
-require 'alephant/sequencer'
-require 'alephant/broker/cache'
+require "crimp"
+require "alephant/cache"
+require "alephant/lookup"
+require "alephant/broker/errors/invalid_cache_key"
+require "alephant/sequencer"
+require "alephant/broker/cache"
 
 module Alephant
   module Broker
@@ -14,32 +14,42 @@ module Alephant
         @id        = meta.id
         @batch_id  = meta.batch_id
         @options   = symbolize(meta.options || {})
-        @content   = data[:content].force_encoding 'UTF-8'
+        @content   = data[:content].force_encoding "UTF-8"
         @opts_hash = meta.opts_hash
         @data      = data
         @meta      = meta
       end
 
       def content_type
-        headers['Content-Type']
+        headers["Content-Type"]
       end
 
       def headers
         {
-          'Content-Type' => data[:content_type].to_s,
-        }.merge(data[:headers] || {})
+          "Content-Type" => data[:content_type].to_s
+        }
+          .merge(data[:headers] || {})
+          .merge(stripped_headers)
       end
 
       def status
-        200
+        meta_data_headers.key?("Status") ? meta_data_headers["Status"] : 200
       end
 
       private
 
       attr_reader :meta, :data
 
+      def meta_data_headers
+        @meta_data_headers ||= data[:meta].fetch(:headers, {})
+      end
+
+      def stripped_headers
+        meta_data_headers.reject { |k, _| k == "Status" }
+      end
+
       def symbolize(hash)
-        Hash[hash.map { |k,v| [k.to_sym, v] }]
+        Hash[hash.map { |k, v| [k.to_sym, v] }]
       end
     end
   end
