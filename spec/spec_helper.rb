92853b204f371263f1d22fd3d96ed2c321fa95ff
Darren Oakley <daz.oakley@gmail.com>
2016-05-17 10:37:42 +0100

    Add simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,11 @@
 $: << File.join(File.dirname(__FILE__), '..', 'lib')
 
+require 'simplecov'
+
+SimpleCov.start do
+  add_filter '/spec/'
+end
+
 require 'pry'
 require 'json'
 require 'alephant/broker'
