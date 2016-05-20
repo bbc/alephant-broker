b3ee8694db99991fe3a8fee3e4a19ed4019bfca9
Darren Oakley <daz.oakley@gmail.com>
2016-05-20 18:29:21 +0100

    WIP

--- lib/alephant/broker/load_strategy.rb
+++ lib/alephant/broker/load_strategy.rb
@@ -4,6 +4,7 @@ module Alephant
       require "alephant/broker/load_strategy/http"
       require "alephant/broker/load_strategy/s3/archived"
       require "alephant/broker/load_strategy/s3/sequenced"
+      require "alephant/broker/load_strategy/revalidate"
     end
   end
 end
