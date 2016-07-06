e617395191555ae3da8f6c15afa41a454a9b0929
Darren Oakley <daz.oakley@gmail.com>
2016-07-06 17:23:00 +0100

    Update the refresh logic
    if the content has expired…
    
    * check S3 for new content
      * if fresh, replace cache
      * if stale, kick off refresher

--- lib/alephant/broker/load_strategy/revalidate/strategy.rb
+++ lib/alephant/broker/load_strategy/revalidate/strategy.rb
@@ -12,16 +12,18 @@ module Alephant
         class Strategy
           include Logger
 
+          STORAGE_ERRORS = [Alephant::Broker::Errors::ContentNotFound].freeze
+
           def load(component_meta)
             loaded_content = cached_object(component_meta)
 
-            refresh_content(component_meta) if loaded_content.expired?
+            update_content(component_meta) if loaded_content.expired?
 
             data = loaded_content.to_h
             data.fetch(:meta, {})[:status] = 200
             data
-          rescue Alephant::Broker::Errors::ContentNotFound
-            refresh_content(component_meta)
+          rescue *STORAGE_ERRORS
+            update_content(component_meta)
 
             {
               content:      '',
@@ -38,18 +40,44 @@ module Alephant
             @cache ||= Cache::Client.new
           end
 
-          def refresh_content(component_meta)
-            Thread.new do
-              logger.info(msg: "#{self.class}#refresh_content - Loading new content from thread")
-              Refresher.new(component_meta).refresh
-            end
-          end
-
           def cached_object(component_meta)
             cache.get(component_meta.component_key) do
               logger.info(msg: "#{self.class}#cached_object - No cache so loading and adding cache object")
               Fetcher.new(component_meta).fetch
             end
+          end
+
+          def update_content(component_meta)
+            Thread.new do
+              stored_content = fetch_stored_content(component_meta)
+
+              if stored_content && !stored_content.expired?
+                cache_new_content(component_meta, stored_content)
+              else
+                refresh_content(component_meta)
+              end
+            end
+          end
+
+          def fetch_stored_content(component_meta)
+            Fetcher.new(component_meta).fetch
+          rescue *STORAGE_ERRORS
+            nil
+          end
+
+          def cache_new_content(component_meta, new_content)
+            logger.info(event:  'NewContentFromS3',
+                        key:    component_meta.component_key,
+                        val:    new_content,
+                        method: "#{self.class}#refresh_content")
+
+            cache.set(component_meta.component_key, new_content)
+          end
+
+          def refresh_content(component_meta)
+            logger.info(msg: "#{self.class}#refresh_content - Loading new content from thread")
+
+            Refresher.new(component_meta).refresh
           end
         end
       end
--- spec/alephant/broker/load_strategy/revalidate/strategy_spec.rb
+++ spec/alephant/broker/load_strategy/revalidate/strategy_spec.rb
@@ -3,10 +3,10 @@ require 'spec_helper'
 RSpec.describe Alephant::Broker::LoadStrategy::Revalidate::Strategy do
   subject { described_class.new }
 
-  let(:lookup_double)  { instance_double(Alephant::Lookup::LookupHelper) }
-  let(:storage_double) { instance_double(Alephant::Storage) }
+  let(:lookup_double)    { instance_double(Alephant::Lookup::LookupHelper) }
+  let(:storage_double)   { instance_double(Alephant::Storage) }
   let(:refresher_double) { instance_double(Alephant::Broker::LoadStrategy::Revalidate::Refresher) }
-  let(:fetcher_double) { instance_double(Alephant::Broker::LoadStrategy::Revalidate::Fetcher) }
+  let(:fetcher_double)   { instance_double(Alephant::Broker::LoadStrategy::Revalidate::Fetcher) }
 
   let(:content) do
     {
@@ -72,23 +72,62 @@ RSpec.describe Alephant::Broker::LoadStrategy::Revalidate::Strategy do
         before do
           allow(cached_obj).to receive(:expired?).and_return(true)
 
-          expect(Alephant::Broker::LoadStrategy::Revalidate::Refresher)
+          allow(Alephant::Broker::LoadStrategy::Revalidate::Refresher)
             .to receive(:new)
             .with(component_meta)
             .and_return(refresher_double)
 
-          allow(refresher_double)
-            .to receive(:refresh)
+          allow(refresher_double).to receive(:refresh)
+
+          allow(Alephant::Broker::LoadStrategy::Revalidate::Fetcher)
+            .to receive(:new)
+            .with(component_meta)
+            .and_return(fetcher_double)
+
+          allow(fetcher_double).to receive(:fetch).and_return(cached_obj)
         end
 
-        it 'gets fetched from the cache and returned' do
+        it 'it gets fetched from the cache and returned to the user' do
           expect(subject.load(component_meta)).to eq(expected_content)
         end
 
-        it 'kicks off a refresh of the content' do
-          expect(refresher_double).to receive(:refresh)
+        context 'in the background...' do
+          let(:new_content)    { { id: 'test', batch_id: '', meta: {} } }
+          let(:new_cached_obj) { Alephant::Broker::Cache::CachedObject.new(new_content) }
 
-          subject.load(component_meta)
+          it 'checks the fetcher, to see if there is newer content (in S3)' do
+            expect(fetcher_double).to receive(:fetch).and_return(new_cached_obj)
+
+            subject.load(component_meta)
+          end
+
+          context 'when there IS newer, non-expired content' do
+            let(:cache) { subject.send(:cache) }
+
+            before do
+              expect(fetcher_double).to receive(:fetch).and_return(new_cached_obj)
+              expect(new_cached_obj).to receive(:expired?).and_return(false)
+            end
+
+            it 'replaces the cached content' do
+              expect(cache).to receive(:set).with(component_meta.component_key, new_cached_obj)
+
+              subject.load(component_meta)
+            end
+          end
+
+          context 'when there IS NOT newer content' do
+            before do
+              expect(fetcher_double).to receive(:fetch).and_return(new_cached_obj)
+              expect(new_cached_obj).to receive(:expired?).and_return(true)
+            end
+
+            it 'kicks off a refresh of the content (from the renderer)' do
+              expect(refresher_double).to receive(:refresh)
+
+              subject.load(component_meta)
+            end
+          end
         end
       end
     end
